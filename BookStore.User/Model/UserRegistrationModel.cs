2b5c427294c6804c9117841e4b7fb68a083f84d5
“karandepale” <karandepale111@gmail.com>
2023-08-28 16:21:22 +0530

    [Karan] Add : Add model for User Registration

---
+++ BookStore.User/Model/UserRegistrationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.User.Model
+{
+    public class UserRegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+    }
+}
