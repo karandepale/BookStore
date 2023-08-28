45ab9892f2b1fe652d2169278cd7dec29475a058
“karandepale” <karandepale111@gmail.com>
2023-08-28 16:04:12 +0530

    [Karan] Add : DataBase created for User Module using code first approach

---
+++ BookStore.User/Context/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.User.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.User.Context
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<UserEntity> User { get; set; }
+    }
+}
