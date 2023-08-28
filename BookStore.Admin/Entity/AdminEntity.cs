2b5d7fa3027a8e97a27f15b794ea800ed948da9a
“karandepale” <karandepale111@gmail.com>
2023-08-28 14:14:27 +0530

    [Karan] Add : Database Created For Admin module in Bookstore  Project using code first approach

---
+++ BookStore.Admin/Entity/AdminEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Admin.Entity
+{
+    public class AdminEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long AdminID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
