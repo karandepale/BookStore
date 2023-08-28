1902c64f3c1942b1655a69bbc4f45a2bade21df2
“karandepale” <karandepale111@gmail.com>
2023-08-28 16:17:44 +0530

    [Karan] Feature : Implement JWT Authentication Feature for User

--- BookStore.User/Program.cs
+++ BookStore.User/Program.cs
@@ -1,5 +1,8 @@
 using BookStore.User.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure JWT settings
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
 builder.Services.AddDbContext<UserContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("UserDB"));
---
+++ BookStore.User/Services/UserRepo.cs
@@ -0,0 +1,37 @@
+﻿using BookStore.User.Context;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BookStore.User.Services
+{
+    public class UserRepo
+    {
+        private readonly UserContext context;
+        private readonly IConfiguration configuration;
+        public UserRepo(UserContext context, IConfiguration configuration)
+        {
+            this.context = context;
+            this.configuration = configuration;
+        }
+
+
+        // JWT TOKEN GENERATE:-
+        public string GenerateJwtToken(string Email, long UserID)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("UserID", UserID.ToString()),
+                new Claim(ClaimTypes.Email, Email),
+        };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(configuration["JwtSettings:Issuer"], configuration["JwtSettings:Audience"], claims, DateTime.Now, DateTime.Now.AddHours(1), creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+}
