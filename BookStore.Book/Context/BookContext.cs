9f924fd61bae4bfa255445a90ff03ae3fff2f5fc
“karandepale” <karandepale111@gmail.com>
2023-08-28 21:44:45 +0530

    [Karan] Setup : Database created for Book Module

---
+++ BookStore.Book/Context/BookContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Book.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Book.Context
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<BookEntity> Book { get; set; }
+    }
+}
--- BookStore.Book/Program.cs
+++ BookStore.Book/Program.cs
@@ -1,3 +1,6 @@
+using BookStore.Book.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BookContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookDB"));
+});
 
 var app = builder.Build();
 
