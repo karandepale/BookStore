2d19a778eb1463401c34f5eea3d6466504dc855a
“karandepale” <karandepale111@gmail.com>
2023-08-29 11:08:02 +0530

    [Karan] Setup : Database Created for Order Module

--- BookStore.Order/Program.cs
+++ BookStore.Order/Program.cs
@@ -1,3 +1,6 @@
+using BookStore.Order.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<OrderContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OrderDB"));
+});
+
+
 
 var app = builder.Build();
 
